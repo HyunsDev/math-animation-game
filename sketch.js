0f5b7fb0fb7841a03863a8c28a6a73c272349bc9
혀느현스 <hyuns@hyuns.dev>
2021-12-26 00:26:57 +0900

    .

--- sketch.js
+++ sketch.js
@@ -69,11 +69,11 @@ const sketch = function (p) {
 
         if (character.x_speed == 0 && character.y_speed == 0 && character.x_accel == 0 && character.y_accel == 0 ) {
             if (character.x_direction == "left") {
-                p.image(animation.still[isStand() ? 0 : p.frameCount%10], realX(character.x), realY(character.y), 50, 60)
+                p.image(animation.still[p.frameCount%10], realX(character.x), realY(character.y), 50, 60)
             } else {
                 p.push()
                 p.scale(-1, 1)
-                p.image(animation.still[isStand() ? 0 : p.frameCount%10], realX(character.x) * -1 - 70, realY(character.y), 50, 60)
+                p.image(animation.still[p.frameCount%10], realX(character.x) * -1 - 70, realY(character.y), 50, 60)
                 p.pop()
             }
         } else {
