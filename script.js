228167256d7e445b463a91773eb57949c79d75b7
혀느현스 <hyuns@hyuns.dev>
2021-12-25 23:03:00 +0900

    .

---
+++ script.js
@@ -0,0 +1,109 @@
+let canvas = new p5(sketch, "canvas");
+const reset = () => {
+    character = {
+        x: 0,
+        y: 0,
+        x_speed: 0,
+        y_speed: 0,
+        x_accel: 0,
+        y_accel: 0,
+        x_direction: "right",
+        y_direction: "front",
+        isWalk: false
+    }
+
+    animation = {
+        still: [],
+        walk: [],
+        walk_back: []
+    }
+
+    history = []
+
+    canvas.remove()
+    canvas = new p5(sketch, "canvas");
+}
+
+accel = 2
+
+
+// 조작 (모바일)
+$(".btn").on("contextmenu", event => {
+    event.preventDefault()
+})
+
+$(".btn").click(event => {
+    character.x_accel = 0
+    character.y_accel = 0
+    for (i of event.currentTarget.id.split("-")) {
+        switch (i) {
+            case "top":
+                character.y_accel = -accel;
+                character.y_direction = "behind";
+                break
+            case "right":
+                character.x_accel = accel;
+                character.x_direction = "right";
+                break
+            case "bottom":
+                character.y_accel = accel;
+                character.y_direction = "front";
+                break
+            case "left":
+                character.x_accel = -accel;
+                character.x_direction = "left";
+                break
+            case "stop":
+                // character.x_speed = 0
+                // character.y_speed = 0
+        }
+    }
+})
+
+
+// 조작 (PC)
+function isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+if (!isMobile()) {
+    $("#btns").hide()
+}
+
+$("body").keydown(event => {
+    switch(event.keyCode) {
+        case 38: // 위 방향키
+            character.y_accel = -accel;
+            character.y_direction = "behind";
+            break
+        case 37: // 왼쪽 방향키
+            character.x_accel = -accel;
+            character.x_direction = "left";
+            break
+        case 39: // 오른쪽 방향키
+            character.x_accel = accel;
+            character.x_direction = "right";
+            break
+        case 40: // 아래 방향키
+            character.y_accel = accel;
+            character.y_direction = "front";
+            break
+    }
+})
+
+$("body").keyup(event => {
+    switch(event.keyCode) {
+        case 38: // 위 방향키
+            character.y_accel = 0;
+            break
+        case 37: // 왼쪽 방향키
+            character.x_accel = 0;
+            break
+        case 39: // 오른쪽 방향키
+            character.x_accel = 0;
+            break
+        case 40: // 아래 방향키
+            character.y_accel = 0;
+            break
+    }
+})
---
+++ sketch.js
@@ -0,0 +1,98 @@
+let character = {
+    x: 0,
+    y: 0,
+    x_speed: 0,
+    y_speed: 0,
+    x_accel: 0,
+    y_accel: 0,
+    x_direction: "right",
+    y_direction: "front",
+    isWalk: false
+}
+
+const realX = (x) => $(window).width()/2 - 20 + x
+const realY = (y) => $(window).height()/2 - 100 + y
+
+// =========[ config ]=========
+const speed_limit = {
+    x: 10,
+    y: 10
+}
+const friction = 0.5
+
+const lineSync = {
+    x: 40,
+    y: 50
+}
+
+// -===========================
+
+let animation = {
+    still: [],
+    walk: [],
+    walk_back: []
+}
+
+let history = []
+
+const sketch = function (p) {
+    p.preload = function() {
+        for (i of Object.keys(animation)) {
+            for (let ii = 0; ii < 10; ii++) {
+                console.log(`[Load Image] ./assets/${i}/${ii}.png`)
+                animation[i].push(p.loadImage(`./assets/${i}/${ii}.png`))
+            }
+        }
+    }
+
+    p.setup = function () {
+        p.createCanvas($(window).width(), $( window ).height())
+        p.frameRate(10)
+    }
+
+    p.draw = function () {
+        p.background("rgb(233, 236, 255)");
+        p.strokeWeight(20);
+        p.stroke("rgb(213, 219, 255)");
+
+        let last_history = {x: 0, y: 0}
+        history.forEach(e => {
+            p.line(realX(last_history.x)+lineSync.x, realY(last_history.y)+lineSync.y, realX(e.x)+lineSync.x, realY(e.y)+lineSync.y)
+            last_history.x = e.x
+            last_history.y = e.y
+        })
+
+        if (character.x_speed == 0 && character.y_speed == 0 && character.x_accel == 0 && character.y_accel == 0 ) {
+            if (character.x_direction == "left") {
+                p.image(animation.still[p.frameCount%10], realX(character.x), realY(character.y), 60, 60)
+            } else {
+                p.push()
+                p.scale(-1, 1)
+                p.image(animation.still[p.frameCount%10], realX(character.x) * -1 - 70, realY(character.y), 60, 60)
+                p.pop()
+            }
+        } else {
+            if (character.x_direction == "left") {
+                p.image(animation[character.y_direction==="front" ? "walk" : "walk_back"][p.frameCount%10], $(window).width()/2 - 20 + character.x, $(window).height()/2 - 100 + character.y, 60, 60)
+            } else {
+                p.push()
+                p.scale(-1, 1)
+                p.image(animation[character.y_direction==="front" ? "walk" : "walk_back"][p.frameCount%10], ($(window).width()/2 - 20 + character.x) * -1 - 70, $(window).height()/2 - 100 + character.y, 60, 60)
+                p.pop()
+            }
+        }
+
+
+        character.x_speed += (Math.abs(character.x_speed + character.x_accel) <= speed_limit.x ? character.x_accel : 0)
+        character.y_speed += (Math.abs(character.y_speed + character.y_accel) <= speed_limit.y ? character.y_accel : 0)
+        character.x += (realX(character.x)+character.x_speed+40 >= 0 && realX(character.x)+character.x_speed+40 <= $(window).width()) ? character.x_speed : 0
+        character.y += (realY(character.y)+character.y_speed+40 >= 0 && realY(character.y)+character.y_speed+50 <= $(window).height()) ? character.y_speed : 0
+        history.push({x: character.x, y: character.y})
+
+        $("#result").html(`<i class="ph-gauge"></i> ${Math.sqrt(character.x_speed**2 + character.y_speed**2)}cm/s`)
+
+
+        // DEBUG
+        $("#tex").html(`(${character.x_accel}, ${character.y_accel}) (${character.x_speed}, ${character.y_speed}) (${character.x}, ${character.y})`)
+    }
+}
